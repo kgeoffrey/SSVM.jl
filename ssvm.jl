52f0949407a84d2c9b187e39b4b8024bdc9046b4
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-12-16 17:38:39 +0100

    Update ssvm.jl

--- ssvm.jl
+++ ssvm.jl
@@ -1,4 +1,6 @@
 ### test smooth ssvm
+using HigherOrderDerivatives
+using Plots
 
 
 X = rand(100, 2)
@@ -7,46 +9,45 @@ Y = rand(range(-1, step = 2, 1), 100)
 
 p(x, α) = x + 1/α * log(1 + exp(-α*x))
 
-function ssvm(X::AbstractArray, Y::Real, w::AbstractArray, v::Real, γ::Real, α = 100)
+function ssvm(X::AbstractArray, Y::AbstractArray, w::AbstractArray, v::Real, γ::Number, α = 100)
     return v/2 * sum((p.(1 .- Y.*(X*w .- γ), α)).^2) + 1/2*(w'*w + γ.^2)
 end
 
-using Plots
+
 
 w = rand(size(X,2))
-γ = 0.5
-ssvm(X,Y, w, 1, 1)
 
-using HigherOrderDerivatives'
+
+ssvm(X, Y, w, 1, 1)
+
 
 
 
 function optimize(X, Y, v, epochs, stepsize)
     w = rand(size(X,2))
-    γ = 0.5
+    γ = 1
 
     loss = []
     for i in 1:epochs
-        t1 = w -> ssvm(X, Y, w, v, 100, γ)
-        t2 =  γ -> ssvm(X, Y, w, v, 100, γ)
+        t1 = w -> ssvm(X, Y, w, v, γ)
+        t2 =  γ -> ssvm(X, Y, w, v, γ)
 
-        w = w - stepsize * gradient(t1, w)
-        γ = γ - stepsize * derivative(t2, γ)
+        w = w - stepsize * gradient(t1, w) # hessian(t1, w) \ gradient(t1, w)
+        γ = γ - stepsize * derivative(t2, γ) #  / derivative(t2, γ, 2)
 
-        append!(loss, ssvm(X, Y, w, v, 100, γ))
+        append!(loss, ssvm(X, Y, w, v, γ))
     end
 
     return loss
 end
 
 
+v = 0.1
+t1 = w -> ssvm(X, Y, w, v, 100, γ)
+t2 =  γ -> ssvm(X, Y, w, v, 100, γ)
 
-gradient(t1, w)
-derivative(t2 ,  γ)
 
 
 test = optimize(X, Y, 0.1, 100, 0.01)
 
 plot(test)
-
-test
