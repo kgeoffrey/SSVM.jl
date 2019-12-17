150d0210779cfeecd16404b36b3d5a4784f59e14
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-12-17 17:43:20 +0100

    Update ssvm.jl

--- ssvm.jl
+++ ssvm.jl
@@ -4,20 +4,19 @@ using Plots
 using StatsBase
 using CSV
 
+# synthetic data
 X = rand(100, 2)
 Y = rand(range(-1, step = 2, 1), 100)
-
-
+w = rand(size(X,2))
+## functions
 p(x, α) = x + 1/α * log(1 + exp(-α*x))
 
 function ssvm(X::AbstractArray, Y::AbstractArray, w::AbstractArray, v::Real, γ::Number, α = 100)
     return v/2 * sum((p.(1 .- Y.*(X*w .- γ), α)).^2) + 1/2*(w'*w + γ.^2)
 end
 
-
-w = rand(size(X,2))
+## testing
 ssvm(X, Y, w, 1, 1)
-
 
 
 function optimize(X, Y, v, epochs, stepsize)
@@ -56,21 +55,30 @@ function split(df, samplesize)
     df_train = df[1:l-s, :]
     df_test = df[l-s:end, :]
     X_train = df_train[:,1:end .!= 5]
-    Y_train = df_train[:,5]
+    Y_train = [if i == 0 (-1) else 1 end for i in df_train[:,5]]
     X_test = df_test[:,1:end .!= 5]
-    Y_test = df_test[:,5]
+    Y_test = [if i == 0 (-1) else 1 end for i in df_test[:,5]]
     return X_test, Y_test, X_train, Y_train
 end
 
 X_test, Y_test, X_train, Y_train = split(df, .20)
 
 
-test, we, g = optimize(X_train, Y_train, 0.1, 20, 0.1)
+test, we, g = optimize(X_train, Y_train, 0.1, 50, 0.1)
 
 plot(test)
 
 pred = X_test*we .- g
 
-[-1 for i in pred if i < 0]
+npred = [if i > 0 1 else (-1.) end for i in pred]
 
-npred = [if i < 0 -1. else 1. end for i in pred]
+
+function predict(SVM, X_test)
+    pred = X_test*we .- g
+    pred = [if i > 0 (1.) else (-1.) end for i in pred]
+    return pred
+end
+
+function accuracy(SVM, Y_test)
+    return 100 - sum(npred - Y_test)/length(npred)
+end^sf
